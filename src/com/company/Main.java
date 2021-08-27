a10a0dcbffe86d17adf7920c73096237719bfd8a
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2021-08-27 21:54:31 +0300

    Work with class string

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String str = "В ногах нет правды. На вкус и цвет товарища нет. На нет и суда нет. Нет дыму без огня. Нет худа без добра. Там хорошо, где нас нет.";
+        System.out.println(str.length());
+    }
+}
