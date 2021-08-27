5645f7801114abea52808f4e3cc1203ceec948a3
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2021-08-27 22:12:03 +0300

    All fine!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,22 @@ public class Main {
     public static void main(String[] args) {
 	    String str = "В ногах нет правды. На вкус и цвет товарища нет. На нет и суда нет. Нет дыму без огня. Нет худа без добра. Там хорошо, где нас нет.";
         System.out.println(str.length());
+        // разбиваем на подстроки и записываем в массив
+        String[] sentence = str.split("\\.");
+        for(String val:sentence){
+            System.out.println(val);
+        }
+        // разбиваем на слова и записываем в массив
+        String[] words = str.split(" ");
+        for (String word:words) {
+            System.out.println(word);
+        }
+        System.out.println("____________");
+        for (String fiveWordInWords: words) {
+            if (fiveWordInWords.length() !=6 && fiveWordInWords.length()>4){
+                System.out.println(fiveWordInWords);
+            }
+        }
+        System.out.println("____________");
     }
 }
